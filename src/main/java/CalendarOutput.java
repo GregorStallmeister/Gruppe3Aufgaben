d625b686b0ccdcd38c0da023feaeee2d1b422163
Manuel Simon <info@der-supernerd.de>
2025-01-28 16:21:52 +0100

    add printWeekdays

--- src/main/java/CalendarOutput.java
+++ src/main/java/CalendarOutput.java
@@ -11,22 +11,47 @@ public class CalendarOutput {
         String month = "Januar";    // Wir wissen wie's geht, vertraut mir!
         printCalendarForMonth(month);
         printCalendarForMonth("Februar");
+        printWeekdays();
+
         printCalendarForMonth("März");
+        printWeekdays();
+
         printCalendarForMonth("April");
+        printWeekdays();
+
         printCalendarForMonth("Mai");
+        printWeekdays();
+
         printCalendarForMonth("Juni");
+        printWeekdays();
+
         printCalendarForMonth("Juli");
+        printWeekdays();
+
         printCalendarForMonth("August");
+        printWeekdays();
+
         printCalendarForMonth("September");
+        printWeekdays();
+
         printCalendarForMonth("Oktober");
+        printWeekdays();
+
         printCalendarForMonth("November");
+        printWeekdays();
+
         printCalendarForMonth("Dezember");
+        printWeekdays();
 
     }
 
  public static void printCalendarForMonth(String monthName) {
      System.out.println("Kalender für " + monthName );
 
+    }
+
+    public static void printWeekdays() {
+        System.out.println("Mo Di Mi Do Fr Sa So");
     }
 }
 
