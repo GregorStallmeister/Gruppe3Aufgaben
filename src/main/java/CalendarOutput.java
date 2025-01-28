4ae614cb30a74e1c527e764524974660355f492d
GregorStallmeister <post@gregor-stallmeister.de>
2025-01-28 15:58:31 +0100

    started calendar output "project"

---
+++ src/main/java/CalendarOutput.java
@@ -0,0 +1,24 @@
+public class CalendarOutput {
+
+    public static void main (String[] args) {
+        System.out.println("Januar");
+//        System.out.println("Mo Di Mi Do Fr Sa So");
+//        System.out.println(" 1  2  3  4  5  6  7");
+//        System.out.println(" 8  9 10 11 12 13 14");
+//        System.out.println("15 16 17 18 19 20 21");
+//        System.out.println("22 23 24 25 26 27 28");
+//        System.out.println("29 30 31");
+
+        System.out.println("Februar");
+        System.out.println("März");
+        System.out.println("April");
+        System.out.println("Mai");
+        System.out.println("Juni");
+        System.out.println("Juli");
+        System.out.println("August");
+        System.out.println("September");
+        System.out.println("Oktober");
+        System.out.println("November");
+        System.out.println("Dezember");
+    }
+}
